3d4e79b09c49f6e0e53c6b4d0c3da009d11e6611
LizPeng <lizhipeng2013@163.com>
2017-09-14 17:01:46 +0800

    回家做比较

--- test/expect/rule.js
+++ test/expect/rule.js
@@ -1,13 +1,13 @@
 module.exports = {
   type: 'stylesheet',
   stylesheet: {
-    rules: [
-      {
+    rules: [ //rulesList = rules(),清除起始空格；node = rule() 然后push到rules里面
+      {//rule包含一个type,selectors,declarations
         type: 'rule',
-        selectors: '.hot',
-        declarations: [
-          {
-            type: 'declaration',
+        selectors: '.hot',//sel = selector()
+        declarations: [//declarations()
+          {//declaration()
+            type: 'declaration',//match匹配
             property: 'color',
             value: 'red'
           },
